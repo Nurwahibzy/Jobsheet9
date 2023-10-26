56601983d3d5a11255889cc6345a98115ed84807
Nurwahibzy <nurwahibzakkiy@gmail.com>
2023-10-26 11:11:29 +0700

    Menambahkan output jumlah mahasiswa yang lulus

--- ArrayRataNilai21.java
+++ ArrayRataNilai21.java
@@ -6,6 +6,7 @@ public class ArrayRataNilai21 {
     public static void main(String[] args) {
         Scanner scan21 = new Scanner(System.in);
         int[] nilaiMhs = new int[10];
+        int mhsLulus = 0;
         double total = 0;
         double rata2;
         for (int i = 0; i < nilaiMhs.length; i++) {
@@ -14,8 +15,12 @@ public class ArrayRataNilai21 {
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                mhsLulus += 1;
+            }
         }
         rata2 = total / nilaiMhs.length;
+        System.out.println("Jumlah mahasiswa lulus = " + mhsLulus);
         System.out.print("Rata-rata nilai = " + rata2);
 
     }

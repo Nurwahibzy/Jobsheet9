611c07936e97cfbd832e2948dad87250f3abf105
Nurwahibzy <nurwahibzakkiy@gmail.com>
2023-10-26 14:26:41 +0700

    Menambahkan pencarian key

--- LinearSearch21.java
+++ LinearSearch21.java
@@ -1,19 +1,32 @@
 package Jobsheet9;
 
+import java.util.Scanner;
+
 public class LinearSearch21 {
     public static void main(String[] args) {
+        Scanner scan21 = new Scanner(System.in);
+        int jmlElemen, key = 0, hasil = 0;
 
-        int[] ArrayInt = { 34, 18, 26, 48, 72, 20, 56, 63 };
-        int key = 20;
-        int hasil = 0;
+        System.out.print("Masukkan jumlah elemen : ");
+        jmlElemen = scan21.nextInt();
 
-        for (int i = 0; i < ArrayInt.length; i++) {
-            if (ArrayInt[i] == key) {
-                hasil = i;
+        int[] arrayint = new int[jmlElemen];
+
+        for (int i = 0; i < arrayint.length; i++) {
+            System.out.print("Masukkan nilai elemen ke-" + i + " : ");
+            arrayint[i] = scan21.nextInt();
+        }
+        System.out.print("Masukkan key yang dicari : ");
+        key = scan21.nextInt();
+        for (int j = 0; j < arrayint.length;j++) {
+            if (arrayint[j] == key) {
+                hasil = j;
+                System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+                break;
+            } else{
+                System.out.println("Key tidak ditemukan");
                 break;
             }
         }
-
-        System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
     }
 }
